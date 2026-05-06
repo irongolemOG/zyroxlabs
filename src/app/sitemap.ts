82b45a5c3771a43c9fb8402896205e4f820bb8a5
irongolemOG <ironicalgolem@gmail.com>
2026-05-06 12:58:19 +0530

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,36 +1,34 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://zyroxlabs.in'
-  
   return [
     {
-      url: `${baseUrl}`,
-      lastModified: new Date(),
+      url: 'https://www.zyroxlabs.in/',
+      lastModified: new Date('2026-05-05T12:43:35.245Z'),
       changeFrequency: 'weekly',
-      priority: 1,
+      priority: 1.0,
     },
     {
-      url: `${baseUrl}/about`,
-      lastModified: new Date(),
+      url: 'https://www.zyroxlabs.in/about',
+      lastModified: new Date('2026-05-05T12:43:35.245Z'),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: `${baseUrl}/services`,
-      lastModified: new Date(),
+      url: 'https://www.zyroxlabs.in/services',
+      lastModified: new Date('2026-05-05T12:43:35.245Z'),
       changeFrequency: 'monthly',
       priority: 0.9,
     },
     {
-      url: `${baseUrl}/our-works`,
-      lastModified: new Date(),
+      url: 'https://www.zyroxlabs.in/our-works',
+      lastModified: new Date('2026-05-05T12:43:35.245Z'),
       changeFrequency: 'weekly',
       priority: 0.8,
     },
     {
-      url: `${baseUrl}/contact`,
-      lastModified: new Date(),
+      url: 'https://www.zyroxlabs.in/contact',
+      lastModified: new Date('2026-05-05T12:43:35.245Z'),
       changeFrequency: 'monthly',
       priority: 0.7,
     },
